b1c22cbc8494b07a3022aeffb5d61393aeb6d9b7
Thibaud Colas <thibaudcolas@gmail.com>
2022-03-27 12:09:25 -0400

    feat(rules): implement font-size-relative rule

---
+++ lib/rules/types.ts
@@ -0,0 +1,49 @@
+// To use this:
+// /** @typedef { import("./types").RuleSchema } RuleSchema */
+
+interface WrappedRuleOption {
+	[key: string]: (number | string)[];
+}
+
+interface WrappedRule {
+	ruleName: string;
+	options: WrappedRuleOption[];
+}
+
+interface SecondaryRuleOption {
+	possible: {
+		[key: string]: any[];
+	};
+	optional: boolean;
+	default: {
+		[key: string]: any;
+	};
+}
+
+interface TestCase {
+	label: string;
+	code: string;
+	demo?: boolean;
+	message: string;
+	line: number;
+	column: number;
+}
+
+interface TestSuite {
+	config: [any] | [any, { [key: string]: any }];
+	accept: TestCase[];
+	reject: TestCase[];
+}
+
+export interface RuleSchema {
+	ruleName: string;
+	wrappedRule: WrappedRule;
+	options: [any, SecondaryRuleOption];
+	docs: {
+		description: string;
+		url: string;
+		impact: 'Serious' | 'Moderate' | 'Low';
+		resources: string[];
+	};
+	tests: TestSuite[];
+}
